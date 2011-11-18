ac4767e97f0932cd37d92b98a4eb19d01531a488
cse08 <cse08@47206569-522c-484a-bce3-3336a38e8f77>
2011-11-18 11:35:56 +0000

    git-svn-id: file:///labs/cse04/svn/problem_solving@30 47206569-522c-484a-bce3-3336a38e8f77

--- src/lab/exp9/currentCode.c
+++ src/lab/exp9/currentCode.c
@@ -1,29 +1,20 @@
-	
-		#include<stdio.h>
-int func(int N,int *weight,int S){
-        return 1;
-	if(!S)// If target weight has been achieved, we have found a solution.
-		return 1;
-		
-	if(!N) // We have exhausted all the weights
-		return 0;
-		/* Case1: We consider the weight for the solution
-           Case2: We discard the weight for the solution
-           */
-	return func(N-1,weight,S-weight[N-1])+func(N-1,weight,S); 
-}
+#include<stdio.h>
 main(){
 	int N;
 	scanf("%d",&N);
-	int weight[N],i;
-	// Weight array stores the respective weights
-	for(i=0;i<N ;i++)
-		scanf("%d",weight+i);
-	int Sum;
-	scanf("%d",&Sum);
-	int no_ways = func(N,weight,Sum);
-	if(!no_ways)
-		no_ways = -1;
-	printf("%d\n",no_ways);
+	int arr[N],i,j;
+	for(i=0;i<N;i++)
+		scanf("%d",arr+i);
+	int ans = 0;
+	int best[N];
+	for(i=0;i<N;i++){
+		best[i] = 1;
+		for(j=0;j<i;j++)
+			if(arr[j]<arr[i])
+				best[i] = (best[i]>(best[j]+1))?best[i]:best[j]+1;
+		if(ans<best[i])
+			ans = best[i];
+	}
+	printf("%d\n",ans);
 	return 0;
 }
