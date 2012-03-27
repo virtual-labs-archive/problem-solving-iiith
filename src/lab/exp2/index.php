f8164956ab5b75f1bb52832f2b11ec77837450e0
saransh.khanna <saransh.khanna@47206569-522c-484a-bce3-3336a38e8f77>
2012-03-27 12:57:43 +0000

    relative links changed
    git-svn-id: file:///labs/cse04/svn/problem_solving@62 47206569-522c-484a-bce3-3336a38e8f77

--- src/lab/exp2/index.php
+++ src/lab/exp2/index.php
@@ -38,10 +38,10 @@ $data['nav'] = $nav;
 
 
 $vlab_url = "http://virtual-labs.ac.in/"; 
-$css_js   = "../";
- $lab_url  = "http://".$_SERVER['HTTP_HOST'] . "/labs/cse04/index.php";
-$exp_url  = "http://".$_SERVER['HTTP_HOST'] . "/labs/cse04/exp2/index.php";
-$base_url = "http://".$_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']; 
+$css_js   = "../"; 
+$lab_url  = "../index.php";
+$exp_url  =  "./index.php";
+$base_url = $exp_url;
 
 include('../exp_template.php');
 
