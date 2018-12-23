0d0854db6a9f33c6fa6dde746b7edd3820077d82
Suchithvirtuallab <44540739+Suchithvirtuallab@users.noreply.github.com>
2018-12-23 13:42:26 +0530

    updated quizconfig.js

--- src/lab/exp1/quizconfig.js
+++ src/lab/exp1/quizconfig.js
@@ -10,16 +10,16 @@ var totalquestions=10
 
 //Enter the solutions corresponding to each question:
 var correctchoices=new Array()
-correctchoices[1]='a' //question 1 solution
-correctchoices[2]='b' //question 2 solution, and so on.
-correctchoices[3]='b'
-correctchoices[4]='a'
-correctchoices[5]='c'
-correctchoices[6]='d'
-correctchoices[7]='b'
-correctchoices[8]='c'
-correctchoices[9]='a'
-correctchoices[10]='c'
+correctchoices[1]="a"; //question 1 solution
+correctchoices[2]="b";//question 2 solution, and so on.
+correctchoices[3]="b";
+correctchoices[4]="a";
+correctchoices[5]="c";
+correctchoices[6]="d";
+correctchoices[7]="b";
+correctchoices[8]="c";
+correctchoices[9]="a";
+correctchoices[10]="c";
 
 /////Don't edit beyond here//////////////////////////
 
