e1e21028df7d4295d33eedba57051114b298f949
VinodRathod2209 <44551454+VinodRathod2209@users.noreply.github.com>
2018-12-26 12:16:00 +0530

    Updated keyboard.js
    Fixed bugs

--- src/lab/exp8/edit_area/keyboard.js
+++ src/lab/exp8/edit_area/keyboard.js
@@ -1,4 +1,4 @@
-var EA_keys = {8:"Retour arriere",9:"Tabulation",12:"Milieu (pave numerique)",13:"Entrer",16:"Shift",17:"Ctrl",18:"Alt",19:"Pause",20:"Verr Maj",27:"Esc",32:"Space",33:"Page up",34:"Page down",35:"End",36:"Begin",37:"Left",38:"Up",39:"Right",40:"Down",44:"Impr ecran",45:"Inser",46:"Suppr",91:"Menu Demarrer Windows / touche pomme Mac",92:"Menu Demarrer Windows",93:"Menu contextuel Windows",112:"F1",113:"F2",114:"F3",115:"F4",116:"F5",117:"F6",118:"F7",119:"F8",120:"F9",121:"F10",122:"F11",123:"F12",144:"Verr Num",145:"Arret defil"};
+var EAKeys = {8:"Retour arriere",9:"Tabulation",12:"Milieu (pave numerique)",13:"Entrer",16:"Shift",17:"Ctrl",18:"Alt",19:"Pause",20:"Verr Maj",27:"Esc",32:"Space",33:"Page up",34:"Page down",35:"End",36:"Begin",37:"Left",38:"Up",39:"Right",40:"Down",44:"Impr ecran",45:"Inser",46:"Suppr",91:"Menu Demarrer Windows / touche pomme Mac",92:"Menu Demarrer Windows",93:"Menu contextuel Windows",112:"F1",113:"F2",114:"F3",115:"F4",116:"F5",117:"F6",118:"F7",119:"F8",120:"F9",121:"F10",122:"F11",123:"F12",144:"Verr Num",145:"Arret defil"};
 
 function keyDown(e){
 	if(!e){	// if IE
@@ -9,22 +9,22 @@ function keyDown(e){
 	for(var i in editArea.plugins){
 		if(typeof(editArea.plugins[i].onkeydown)=="function"){
 			if(editArea.plugins[i].onkeydown(e)===false){ // stop propaging
-				if(EditArea.isIE){
+				if(editArea.isIE){
 					e.keyCode=0;}
 				return false;
 			}
 		}
 	}
 
-	var target_id=(e.target || e.srcElement).id;
-	var use=false;
-	if (EA_keys[e.keyCode])
-	{	letter=EA_keys[e.keyCode];
+	var targetId=(e.target || e.srcElement).id;
+	var use=false,letter;
+	if (EAKeys[e.keyCode])
+	{	letter=EAKeys[e.keyCode];
 	}
 	else{
 		letter=String.fromCharCode(e.keyCode);
 	}
-	var low_letter= letter.toLowerCase();
+	var lowLetter= letter.toLowerCase();
 			
 	if(letter=="Page up" && !AltPressed(e) && !editArea.isOpera){
 		editArea.execCommand("scroll_page", {"dir": "up", "shift": ShiftPressed(e)});
@@ -45,17 +45,17 @@ function keyDown(e){
 		use=true;
 		if(editArea.isOpera || (editArea.isFirefox && editArea.isMac) )	// opera && firefox mac can't cancel tabulation events...
 		{	setTimeout("editArea.execCommand('focus');", 1);}
-	}else if(letter=="Entrer" && target_id=="textarea"){
+	}else if(letter=="Entrer" && targetId=="textarea"){
 		if(editArea.press_enter())
 		{	use=true;}
-	}else if(letter=="Entrer" && target_id=="area_search"){
+	}else if(letter=="Entrer" && targetId=="area_search"){
 		editArea.execCommand("area_search");
 		use=true;
 	}else  if(letter=="Esc"){
 		editArea.execCommand("close_all_inline_popup", e);
 		use=true;
 	}else if(CtrlPressed(e) && !AltPressed(e) && !ShiftPressed(e)){
-		switch(low_letter){
+		switch(lowLetter){
 			case "f":				
 				editArea.execCommand("area_search");
 				use=true;
