78bb42e74d92faf28d57a9a996d5880a064815fa
VinodRathod2209 <44551454+VinodRathod2209@users.noreply.github.com>
2018-12-28 14:52:42 +0530

    Updated reg_syntax.js
    Added { } to if else

--- src/lab/exp6/edit_area/reg_syntax.js
+++ src/lab/exp6/edit_area/reg_syntax.js
@@ -2,8 +2,8 @@
 		//res="( |=|\\n|\\r|\\[|\\(|µ|)(";
 		res="(\\b)(";
 		for(i=0; i<text_array.length; i++){
-			if(i>0)
-				res+="|";
+			if(i>0){
+				res+="|";}
 			//res+="("+ tab_text[i] +")";
 			//res+=tab_text[i].replace(/(\.|\?|\*|\+|\\|\(|\)|\[|\]|\{|\})/g, "\\$1");
 			res+=this.get_escaped_regexp(text_array[i]);
@@ -34,7 +34,9 @@
 					if(this.load_syntax[lang]['KEYWORD_CASE_SENSITIVE']===false)
 						param+="i";
 					for(var i in this.load_syntax[lang]['KEYWORDS']){
-						if(typeof(this.load_syntax[lang]['KEYWORDS'][i])=="function") continue;
+						if(typeof(this.load_syntax[lang]['KEYWORDS'][i])=="function"){
+							continue;
+						}
 						this.syntax[lang]["keywords_reg_exp"][i]= new RegExp(this.get_regexp( this.load_syntax[lang]['KEYWORDS'][i] ), param);
 						this.keywords_reg_exp_nb++;
 					}
