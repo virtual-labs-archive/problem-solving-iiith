e14788d466dee1d393d4a9a45693c36c2e575531
VinodRathod2209 <44551454+VinodRathod2209@users.noreply.github.com>
2018-12-29 22:14:29 +0530

    Updated keyBoard.js

--- src/lab/exp8/edit_area/keyboard.js
+++ src/lab/exp8/edit_area/keyboard.js
@@ -1,3 +1,4 @@
+
 var EAKeys = {8:"Retour arriere",9:"Tabulation",12:"Milieu (pave numerique)",13:"Entrer",16:"Shift",17:"Ctrl",18:"Alt",19:"Pause",20:"Verr Maj",27:"Esc",32:"Space",33:"Page up",34:"Page down",35:"End",36:"Begin",37:"Left",38:"Up",39:"Right",40:"Down",44:"Impr ecran",45:"Inser",46:"Suppr",91:"Menu Demarrer Windows / touche pomme Mac",92:"Menu Demarrer Windows",93:"Menu contextuel Windows",112:"F1",113:"F2",114:"F3",115:"F4",116:"F5",117:"F6",118:"F7",119:"F8",120:"F9",121:"F10",122:"F11",123:"F12",144:"Verr Num",145:"Arret defil"};
 	
 function checkWindow(e)
@@ -38,7 +39,7 @@ function altPressed(e) {
 
 function keyDown(e){
 	if(!e){	// if IE
-		e=event;
+		e=window.event;
 	}
 	
 	// send the event to the plugins
@@ -95,6 +96,7 @@ function keyDown(e){
 			case "f":				
 				editarea.execCommand("area_search");
 				use=true;
+				break;
 			case "r":
 				editarea.execCommand("area_replace");
 				use=true;
@@ -139,12 +141,14 @@ function keyDown(e){
 	if(use){
 		// in case of a control that sould'nt be used by IE but that is used => THROW a javascript error that will stop key action
 		if(editarea.isIE)
-			e.keyCode=0
+		{e.keyCode=0}
 		return false;
 	}
 	//alert("Test: "+ letter + " ("+e.keyCode+") ALT: "+ AltPressed(e) + " CTRL "+ CtrlPressed(e) + " SHIFT "+ ShiftPressed(e));
-	
-	return true;
+	else
+	{
+		return true;
+	}
 	
 }
 
