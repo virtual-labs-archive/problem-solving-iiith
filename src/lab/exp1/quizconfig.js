e33c7d56dd4155ee51fa693b4e99eb973fd36d2c
Suchithvirtuallab <44540739+Suchithvirtuallab@users.noreply.github.com>
2018-12-29 12:02:22 +0530

    updated new quizconfig.js

--- src/lab/exp1/quizconfig.js
+++ src/lab/exp1/quizconfig.js
@@ -55,12 +55,12 @@ var win2=window.open("","win2","width=200,height=350, scrollbars")
 win2.focus()
 win2.document.open()
 win2.document.write('<title>Solution</title>')
-win2.document.write('<body bgcolor="#FFFFFF">')
-win2.document.write('<center><h3>Solution to Quiz</h3></center>')
-win2.document.write('<center><font face="Arial">')
-for (i=1;i<=totalquestions;i++){
-for (temp=0;temp<incorrect.length;temp++){
-if (i==incorrect[temp])
+win2.document.write("<body bgcolor="#FFFFFF">");
+win2.document.write("<center><h3>Solution to Quiz</h3></center>");
+win2.document.write("<center><font face="Arial">");
+for (var i=1;i<=totalquestions;i++){
+for (var temp=0;temp<incorrect.length;temp++){
+if (i==incorrect[temp]){
 wrong=1
 }
 if (wrong==1){
