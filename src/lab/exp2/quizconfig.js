059a898d85f3cc065d30d87a7babad107a9fe9b3
alekhyakishore <46134396+alekhyakishore@users.noreply.github.com>
2018-12-29 19:15:55 +0530

    committed changes
    inserted semicolons at required positions

--- src/lab/exp2/quizconfig.js
+++ src/lab/exp2/quizconfig.js
@@ -10,16 +10,16 @@ var totalquestions=10
 
 //Enter the solutions corresponding to each question:
 var correctchoices=new Array()
-correctchoices[1]='a' //question 1 solution
-correctchoices[2]='b' //question 2 solution, and so on.
-correctchoices[3]='b'
-correctchoices[4]='a'
-correctchoices[5]='c'
-correctchoices[6]='d'
-correctchoices[7]='b'
-correctchoices[8]='c'
-correctchoices[9]='a'
-correctchoices[10]='c'
+correctchoices[1]='a'; //question 1 solution
+correctchoices[2]='b';//question 2 solution, and so on.
+correctchoices[3]='b';
+correctchoices[4]='a';
+correctchoices[5]='c';
+correctchoices[6]='d';
+correctchoices[7]='b';
+correctchoices[8]='c';
+correctchoices[9]='a';
+correctchoices[10]='c';
 
 /////Don't edit beyond here//////////////////////////
 
@@ -51,7 +51,7 @@ window.location="results.htm"
 
 
 function showsolution(){
-var win2=window.open("","win2","width=200,height=350, scrollbars")
+var win2=window.open("","win2","width=200,height=350, scrollbars");
 win2.focus()
 win2.document.open()
 win2.document.write('<title>Solution</title>')
