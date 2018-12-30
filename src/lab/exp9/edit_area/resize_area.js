5497cf053af093287f04e8da75b3dd523f7409c1
VinodRathod2209 <44551454+VinodRathod2209@users.noreply.github.com>
2018-12-30 20:47:42 +0530

    Updated resize_area.js

--- src/lab/exp9/edit_area/resize_area.js
+++ src/lab/exp9/edit_area/resize_area.js
@@ -1,4 +1,73 @@
-	function checkEditArea(t)
+
+function EditAreaLoader(){
+	var t=this;
+	t.version= "0.8.2";
+	var date= new Date();
+	t.startTime=date.getTime();
+	t.win= "loading";	// window loading state
+	t.error= false;	// to know if load is interrrupt
+	t.baseURL="";
+	//t.suffix="";
+	t.template="";
+	t.lang= {};	// array of loaded speech language
+	t.load_syntax= {};	// array of loaded syntax language for highlight mode
+	t.syntax= {};	// array of initilized syntax language for highlight mode
+	t.loadedFiles= [];
+	t.waiting_loading= {}; 	// files that must be loaded in order to allow the script to really start
+	// scripts that must be loaded in the iframe
+	t.scripts_to_load= ["elements_functions", "resize_area", "reg_syntax"];
+	t.sub_scripts_to_load= ["edit_area", "manage_area" ,"edit_area_functions", "keyboard", "search_replace", "highlight", "regexp"];
+	t.syntax_display_name= { /*syntax_display_name_AUTO-FILL-BY-COMPRESSOR*/ };
+	
+	t.resize= []; // contain resizing datas
+	t.hidden= {};	// store datas of the hidden textareas
+	
+	t.default_settings= {
+		//id: "src"	// id of the textarea to transform
+		debug: false
+		,smooth_selection: true
+		,font_size: "10"		// not for IE
+		,font_family: "monospace"	// can be "verdana,monospace". Allow non monospace font but Firefox get smaller tabulation with non monospace fonts. IE doesn't change the tabulation width and Opera doesn't take this option into account... 
+		,start_highlight: false	// if start with highlight
+		,toolbar: "search, go_to_line, fullscreen, |, undo, redo, |, select_font,|, change_smooth_selection, highlight, reset_highlight, word_wrap, |, help"
+		,begin_toolbar: ""		//  "new_document, save, load, |"
+		,end_toolbar: ""		// or end_toolbar
+		,is_multi_files: false		// enable the multi file mode (the textarea content is ignored)
+		,allow_resize: "both"	// possible values: "no", "both", "x", "y"
+		,show_line_colors: false	// if the highlight is disabled for the line currently beeing edited (if enabled => heavy CPU use)
+		,min_width: 400
+		,min_height: 125
+		,replace_tab_by_spaces: false
+		,allow_toggle: true		// true or false
+		,language: "en"
+		,syntax: ""
+		,syntax_selection_allow: "basic,brainfuck,c,coldfusion,cpp,css,html,java,js,pas,perl,php,python,ruby,robotstxt,sql,tsql,vb,xml"
+		,display: "onload" 		// onload or later
+		,max_undo: 30
+		,browsers: "known"	// all or known
+		,plugins: "" // comma separated plugin list
+		,gecko_spellcheck: false	// enable/disable by default the gecko_spellcheck
+		,fullscreen: false
+		,is_editable: true
+		,cursor_position: "begin"
+		,word_wrap: false		// define if the text is wrapped of not in the textarea
+		,autocompletion: false	// NOT IMPLEMENTED			
+		,load_callback: ""		// click on load button (function name)
+		,save_callback: ""		// click on save button (function name)
+		,change_callback: ""	// textarea onchange trigger (function name)
+		,submit_callback: ""	// form submited (function name)
+		,EA_init_callback: ""	// EditArea initiliazed (function name)
+		,EA_delete_callback: ""	// EditArea deleted (function name)
+		,EA_load_callback: ""	// EditArea fully loaded and displayed (function name)
+		,EA_unload_callback: ""	// EditArea delete while being displayed (function name)
+		,EA_toggle_on_callback: ""	// EditArea toggled on (function name)
+		,EA_toggle_off_callback: ""	// EditArea toggled off (function name)
+		,EA_file_switch_on_callback: ""	// a new tab is selected (called for the newly selected file)
+		,EA_file_switch_off_callback: ""	// a new tab is selected (called for the previously selected file)
+		,EA_file_close_callback: ""		// close a tab
+	};	
+
+function checkEditArea(t)
 	{
 		if(t.settings["syntax_selection_allow"] && t.settings["syntax_selection_allow"].length>0){
 			t.syntaxList= t.settings["syntax_selection_allow"].replace(/ /g,"").split(",");
@@ -57,7 +126,7 @@
 		t.isResizing=false;
 		t.id= areaId;
 	}
-	function EditArea(){
+	function EditAreas(){
 		var t=this,areaId;
 		t.error= false;	// to know if load is interrrupt
 
@@ -84,19 +153,19 @@
 		t.tabilation=checkEditArea2(t);
 		checkEditArea(t);
 		// retrieve the init parameter for syntax
-		t.settings= EditArea[t.id]["settings"];
+		t.settings= EditAreas[t.id]["settings"];
 	}	
-	editAreaLoader.prototype.endResizeArea= function(e){
+	EditAreaLoader.prototype.endResizeArea= function(e){
 		var d=document,div,a,width,height;
 		
 		d.onmouseup="";
 		d.onmousemove="";		
 		
 		div		= d.getElementById("edit_area_resize");		
-		a= editAreas[editAreaLoader.resize["id"]]["textarea"];
-		width	= Math.max(editAreas[editAreaLoader.resize["id"]]["settings"]["min_width"], div.offsetWidth-4);
-		height	= Math.max(editAreas[editAreaLoader.resize["id"]]["settings"]["min_height"], div.offsetHeight-4);
-		if(editAreaLoader.isIE===6){
+		a= EditAreas[EditAreaLoader.resize["id"]]["textarea"];
+		width	= Math.max(EditAreas[EditAreaLoader.resize["id"]]["settings"]["min_width"], div.offsetWidth-4);
+		height	= Math.max(EditAreas[EditAreaLoader.resize["id"]]["settings"]["min_height"], div.offsetHeight-4);
+		if(EditAreaLoader.isIE===6){
 			width-=2;
 			height-=2;	
 		}
@@ -104,38 +173,68 @@
 		a.style.height		= height+"px";
 		div.style.display	= "none";
 		a.style.display		= "inline";
-		a.selectionStart	= editAreaLoader.resize["selectionStart"];
-		a.selectionEnd		= editAreaLoader.resize["selectionEnd"];
-		editAreaLoader.toggle(editAreaLoader.resize["id"]);
+		a.selectionStart	= EditAreaLoader.resize["selectionStart"];
+		a.selectionEnd		= EditAreaLoader.resize["selectionEnd"];
+		EditAreaLoader.toggle(EditAreaLoader.resize["id"]);
 		
 		return false;
 	};
-	
-	editAreaLoader.prototype.resizeArea= function(e){		
+	function getMouseX(e){
+
+		if(e!=null && typeof(e.pageX) !== "undefined"){
+			return e.pageX;
+		}else{
+			return (e!=null?e.x:event.x)+ document.documentElement.scrollLeft;
+		}
+	};
+	function getMouseY(e){
+		if(e!=null && typeof(e.pageY)!="undefined"){
+			return e.pageY;
+		}else{
+			return (e!=null?e.y:event.y)+ document.documentElement.scrollTop;
+		}
+	};
+	EditAreaLoader.prototype.resizeArea= function(e){		
 		var allow,newHeight,newWidth;
-		allow	= editAreas[editAreaLoader.resize["id"]]["settings"]["allow_resize"];
+		allow	= EditAreas[EditAreaLoader.resize["id"]]["settings"]["allow_resize"];
 		if(allow==="both" || allow==="y")
 		{
-			newHeight	= Math.max(20, getMouseY(e)- editAreaLoader.resize["start_top"]);
+			newHeight	= Math.max(20, getMouseY(e)- EditAreaLoader.resize["start_top"]);
 			document.getElementById("edit_area_resize").style.height= newHeight+"px";
 		}
 		if(allow==="both" || allow==="x")
 		{
-			newWidth= Math.max(20, getMouseX(e)- editAreaLoader.resize["start_left"]);
+			newWidth= Math.max(20, getMouseX(e)- EditAreaLoader.resize["start_left"]);
 			document.getElementById("edit_area_resize").style.width= newWidth+"px";
 		}
 		
 		return false;
 	};
+
+	function calculeOffset(element,attr){
+		var offset=0;
+		while(element){
+			offset+=element[attr];
+			element=element.offsetParent;
+		}
+		return offset;
+	};
+
+	function calculeOffsetLeft(r){
+		return calculeOffset(r,"offsetLeft");
+	};
 	
+	function calculeOffsetTop(r){
+		return calculeOffset(r,"offsetTop");
+	};
 	EditAreaLoader.prototype.startResizeArea= function(){
 		var d=document,a,div,width,height,father;
 		
-		d.onmouseup= editAreaLoader.endResizeArea;
-		d.onmousemove= editAreaLoader.resizeArea;
-		editAreaLoader.toggle(editAreaLoader.resize["id"]);		
+		d.onmouseup= EditAreaLoader.endResizeArea;
+		d.onmousemove= EditAreaLoader.resizeArea;
+		EditAreaLoader.toggle(EditAreaLoader.resize["id"]);		
 		
-		a	= editAreas[editAreaLoader.resize["id"]]["textarea"];
+		a	= EditAreas[EditAreaLoader.resize["id"]]["textarea"];
 		div	= d.getElementById("edit_area_resize");
 		if(!div){
 			div= d.createElement("div");
@@ -153,9 +252,9 @@
 		
 		a.style.display="none";
 				
-		editAreaLoader.resize["start_top"]= calculeOffsetTop(div);
-		editAreaLoader.resize["start_left"]= calculeOffsetLeft(div);		
+		EditAreaLoader.resize["start_top"]= calculeOffsetTop(div);
+		EditAreaLoader.resize["start_left"]= calculeOffsetLeft(div);		
 	};
 	
 	
-	editAreaLoader.waitingLoading["resize_area.js"]= "loaded";
+	EditAreaLoader.waitingLoading["resize_area.js"]= "loaded";
