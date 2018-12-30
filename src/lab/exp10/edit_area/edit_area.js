846e224a2b58e3c81e106bf59b9bee8740a2841e
samhith-25 <44531640+samhith-25@users.noreply.github.com>
2018-12-30 22:51:04 +0530

    Update edit_area.js

--- src/lab/exp10/edit_area/edit_area.js
+++ src/lab/exp10/edit_area/edit_area.js
@@ -86,7 +86,7 @@
 			t.syntax_list= t.settings["syntax_selection_allow"].replace(/ /g,"").split(",");
 		
 		if(t.settings['syntax'])
-			t.allready_used_syntax[t.settings['syntax']]=true;
+		{t.allready_used_syntax[t.settings['syntax']]=true;}
 		
 		
 	};
