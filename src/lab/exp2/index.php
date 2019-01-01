a31dd86e00b8de3f81269a51f7c3374c02bac193
Vikas <44545660+vikasdo@users.noreply.github.com>
2019-01-01 19:15:39 +0530

    Update index.php

--- src/lab/exp2/index.php
+++ src/lab/exp2/index.php
@@ -8,7 +8,6 @@ $html = file_get_html('./content.html');
 if(!isset($_GET["section"])) $section = "Introduction"; 
 else $section = $_GET["section"];
 // Find all images
-
 $labheaderheading =$html->getElementById("experiment-header-heading")->innertext;
 $labarticleheading =$html->getElementById("experiment-article-heading")->innertext;
 		
@@ -29,7 +28,7 @@ $labarticleheading =$html->getElementById("experiment-article-heading")->innerte
 	   // echo "<b>Heading:</b> ".$html->getElementById($element->id."-heading")->innertext."<br>";
 	   // echo "<b>Content: </b>".$html->getElementById($element->id."-content")->innertext."<br><br><br>";
 	 } 
-	   
+	  
 	
 }
 $data['nav'] = $nav;
