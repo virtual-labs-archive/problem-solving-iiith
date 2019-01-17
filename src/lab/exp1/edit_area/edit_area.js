a4bc7b70bf73891618a1e4edae894bd824a7cee9
dheerajskylark <33366164+dheerajskylark@users.noreply.github.com>
2019-01-17 19:13:39 +0530

    Update edit_area.js

--- src/lab/exp1/edit_area/edit_area.js
+++ src/lab/exp1/edit_area/edit_area.js
@@ -10,29 +10,28 @@
 		var t=this;
 		t.error= false;	// to know if load is interrrupt
 		
-		t.inlinePopup= [{popup_id: "area_search_replace", icon_id: "search"},
-									{popup_id: "edit_area_help", icon_id: "help"}];
+		t.inlinePopup= [{popupId: "area_search_replace", iconId: "search"},
+									{popupId: "edit_area_help", iconId: "help"}];
 		t.plugins= {};
 	
-		t.line_number=0;
+		t.lineNumber=0;
 		
-		parent.editAreaLoader.set_browser_infos(t); 	// navigator identification
+		parent.editAreaLoader.setBrowserInfos(t); 	// navigator identification
 		// fix IE8 detection as we run in IE7 emulate mode through X-UA <meta> tag
 		if( t.isIE >= 8 )
-			t.isIE	= 7;
-		
-		t.last_selection={};		
-		t.last_text_to_highlight="";
-		t.last_hightlighted_text= "";
-		t.syntax_list= [];
-		t.allready_used_syntax= {};
-		t.check_line_selection_timer= 50;	// the timer delay for modification and/or selection change detection
+		{t.isIE	= 7;}
+		t.lastSelection={};		
+		t.lastTextToHighlight="";
+		t.lastHightlightedText= "";
+		t.syntaxList= [];
+		t.allreadyUsedSyntax= {};
+		t.checkLineSelectionTimer= 50;	// the timer delay for modification and/or selection change detection
 		
 		t.textareaFocused= false;
-		t.highlight_selection_line= null;
+		t.highlightSelectionLine= null;
 		t.previous= [];
 		t.next= [];
-		t.last_undo="";
+		t.lastUndo="";
 		t.files= {};
 		t.filesIdAssoc= {};
 		t.curr_file= '';
