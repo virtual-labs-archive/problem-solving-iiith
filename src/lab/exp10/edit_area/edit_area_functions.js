b65bcb2f78e7a508e22f9ab0e497f1f0c8985a40
sowmyaracha <46263186+sowmyaracha@users.noreply.github.com>
2019-01-19 15:54:44 +0530

    Update edit_area_functions.js

--- src/lab/exp10/edit_area/edit_area_functions.js
+++ src/lab/exp10/edit_area/edit_area_functions.js
@@ -51,7 +51,7 @@
 			s["font_family"]= family;
 		if(size && size>0)
 			s["font_size"]	= size;
-		if( t.isOpera && t.isOpera < 9.6 )	// opera<9.6 can't manage non monospace font
+		if( t.isOpera && t.isOpera < 9.6 ){	// opera<9.6 can't manage non monospace font
 			s['font_family']="monospace";
 			
 		// update the select tag
