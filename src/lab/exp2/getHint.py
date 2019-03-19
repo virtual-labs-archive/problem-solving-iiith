8448354b65a6c757b5935e2ce10d3089a6cb15fe
Shikhar Garg <shikhar.garg@students.iiit.ac.in>
2019-03-19 19:10:27 +0530

    fixed mistake in previous commit

--- src/lab/exp2/getHint.py
+++ src/lab/exp2/getHint.py
@@ -7,7 +7,7 @@ for x in sfpath[0:-1]:
         PATH=PATH+'/'+x
 PATH=PATH+'/'
 
-def index(req,qNo,hintNo,lang,msid):
+def index(req,qNo,hintNo,lang,sid):
         if(qNo=="1"):
                 dir = PATH + "Hints1/"
         elif(qNo=="2"):
