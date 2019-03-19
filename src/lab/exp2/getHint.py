d3c7019d6b53c0f175af02507f6dc354be894f1e
Shikhar Garg <shikhar.garg@students.iiit.ac.in>
2019-03-19 19:06:13 +0530

    Added cpp hint in exp2

--- src/lab/exp2/getHint.py
+++ src/lab/exp2/getHint.py
@@ -7,17 +7,17 @@ for x in sfpath[0:-1]:
         PATH=PATH+'/'+x
 PATH=PATH+'/'
 
-def index(req,qNo,hintNo,sid):
+def index(req,qNo,hintNo,lang,msid):
         if(qNo=="1"):
                 dir = PATH + "Hints1/"
         elif(qNo=="2"):
                 dir = PATH + "Hints2/"
         try:
-                F = open(dir+"Hint_"+hintNo+".html","r");
+                F = open(dir+"Hint_"+hintNo+lang+".html","r");
 
                 s =  F.read()
                 
                 F.close()
         except:
                 s = 'Failed to load' ;
-	return s;
+        return s;
